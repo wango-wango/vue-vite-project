d2cec14fb1d659ce23b89ee27ab164f16c67c00a
Jerry Wwang <doghost111@gmail.com>
2023-01-05 10:00:10 +0800

    QA page 以及假資料

---
+++ src/views/home/components/QA.vue
@@ -0,0 +1,50 @@
+<script setup>
+import AccordionVue from "../../../components/common/Accordion.vue";
+import { h } from "vue";
+const render = () => {
+  return h("div", null, "常見問題");
+};
+const QAData = [
+  {
+    title: "誰可以網路投保？",
+    content:
+      "法令規定網路投保之要保人年齡資格限定為18歲以上，惟商品投保年齡視各商品規定，且須完成國泰人壽會員註冊始能投保。",
+  },
+  {
+    title: "受益人可以指定給誰？",
+    content: "網路投保身故保險金受益人限被保險人之法定繼承人、直系血親或配偶。",
+  },
+  {
+    title: "要保人、被保險人一定要同一人嗎？",
+    content: "是，此商品要、被保險人須為同一人。",
+  },
+  {
+    title: "若投保後，發現保單不適合該怎麼辦？",
+    content:
+      `由於保險契約大多屬於長期繼續性的契約，投保時應審慎衡量自身需求及繳費能力。要保人可隨時至國泰人壽各服務據點 https://www.cathaylife.com.tw/cathaylife/location，或登入會員專區 https://www.cathaylife.com.tw/oc/OCWeb/html/OC/Login/Login.jsp，提出終止本契約之申請，若要保人保險費已付足達一年以上或繳費累積達有保單價值準備金，國泰人壽應償付解約金。`,
+  },
+];
+</script>
+<template>
+  <div class="QA-container">
+    <div class="QA-title"><render /></div>
+    <div class="QA-content">
+      <AccordionVue :QAList="QAData" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "src/assets/scss/variable.scss";
+.QA-container {
+  padding: 2rem;
+  .QA-title {
+    font-size: 2.5rem;
+    text-align: center;
+    padding: 1rem;
+    font-weight: 600;
+    padding-bottom: 2rem;
+    color: $blue;
+  }
+}
+</style>
